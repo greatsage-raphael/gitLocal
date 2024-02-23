4f084452c8ae8b19b9b3686738c17fa3c15fcc37
greatsage-raphael <bizzicole87@gmail.com>
2024-02-23 19:44:20 +0300

    Fix: supabase url

--- scripts/admin.ts
+++ scripts/admin.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://eifeyuvbxmsjjtbtbyuk.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 
-export const supabaseAdmin = createClient(supabaseUrl!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+export const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
 
 
